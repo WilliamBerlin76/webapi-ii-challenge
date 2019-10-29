2808bf592d823630fa8cde7c275c6f7fd38536ca
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-29 15:04:45 -0500

    get request now working from the router

--- data/postRouter.js
+++ data/postRouter.js
@@ -2,4 +2,20 @@ const router = require('express').Router();
 
 const Posts = require('./db');
 
+router.get('/', (req, res) => {
+
+    const query = req.query;
+
+    Posts.find(query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                message: 'Error retrieving the posts',
+            });
+        });
+});
+
 module.exports = router;
